b7da999547a03ed95d8e790e14fb218a17c972a7
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-01-20 12:00:10 +0100

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject version-clj "0.1.2"
+(defproject version-clj "0.1.3-SNAPSHOT"
   :description "Version Analysis and Comparison for Clojure"
   :url "https://github.com/xsc/version-clj"
   :license {:name "Eclipse Public License"
