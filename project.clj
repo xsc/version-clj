e4cf6084f245d17507f6dbd35a9f2b63f53570ea
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-13 15:58:02 +0100

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject version-clj "1.0.0"
+(defproject version-clj "1.0.1-SNAPSHOT"
   :description "Version Analysis and Comparison for Clojure"
   :url "https://github.com/xsc/version-clj"
   :license {:name "MIT"
