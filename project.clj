d2ab2cb948e5cfbe349b159e1f826a43661e1388
Yannick Scherer <yannick@xsc.dev>
2024-08-20 12:03:53 +0200

    Version 2.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject version-clj "2.0.3-SNAPSHOT"
+(defproject version-clj "2.0.3"
   :description "Version Analysis and Comparison for Clojure"
   :url "https://github.com/xsc/version-clj"
   :license {:name "MIT"
